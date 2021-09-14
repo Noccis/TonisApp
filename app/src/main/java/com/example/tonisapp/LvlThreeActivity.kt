199f9aeda3c90482e502d76b56dd5ba434def0bc
Noccis <89582846+Noccis@users.noreply.github.com>
2021-09-14 15:32:55 +0200

    Första versionen

---
+++ app/src/main/java/com/example/tonisapp/LvlThreeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tonisapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LvlThreeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lvl_three)
+    }
+}
---
+++ app/src/main/java/com/example/tonisapp/LvlTwoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tonisapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LvlTwoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lvl_two)
+    }
+}
