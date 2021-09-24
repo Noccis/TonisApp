74261842362871670aac2af5721906b244607263
Noccis <89582846+Noccis@users.noreply.github.com>
2021-09-24 03:58:55 +0200

    Moved todo list to seperate text file

--- app/src/main/java/com/example/tonisapp/Funtions.kt
+++ app/src/main/java/com/example/tonisapp/Funtions.kt
@@ -1,5 +1,9 @@
 package com.example.tonisapp
 
+import android.provider.Settings.Global.getString
+import android.view.View
+import android.widget.TextView
+
 
 // Main Nr funktioner:
 
@@ -20,3 +24,22 @@ fun setImage(cardList: MutableList<Card>): Int {
 fun random1or2():Int {
     return (1..2).random()
 }
+/*
+fun hiOrLow(lowerView: TextView, higherView: TextView): String {
+
+    hiOrLow = random1or2()
+    val string: String
+    if (hiOrLow == 1) {
+
+        string = getString(R.string.lower)                                                                   // Fixa så den pekar på strängen i XML filen
+        lowerView.visibility = View.VISIBLE
+    } else if (hiOrLow == 2) {
+        string = getString(R.string.higher)                                                                    // Fixa så den pekar på strängen i XML filen
+        higherView.visibility = View.VISIBLE
+    } else {
+        string = getString(R.string.error)
+    }
+    return string
+}
+
+ */
